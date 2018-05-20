10a8980e15b8923c716136fe592c28e98f52bfe4
cefika <cefika99@live.com>
2018-05-20 11:46:57 +0200

    mine

--- app/src/main/java/com/example/kurs/aaaaaaaaaaaaa/Biraj.java
+++ app/src/main/java/com/example/kurs/aaaaaaaaaaaaa/Biraj.java
@@ -3,7 +3,10 @@ package com.example.kurs.aaaaaaaaaaaaa;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Biraj extends AppCompatActivity {
 
@@ -14,9 +17,21 @@ public class Biraj extends AppCompatActivity {
     }
 
     public void igraj(View view) {
-      int broj= Integer.parseInt((String)view.getTag());
+        RadioGroup rg = (RadioGroup) findViewById(R.id.rg1);
+        RadioGroup rg2 = (RadioGroup) findViewById(R.id.rg2);
+        final String value =
+                ((RadioButton)findViewById(rg.getCheckedRadioButtonId()))
+                        .getTag().toString();
+
+
+        final String value2 =
+                ((RadioButton)findViewById(rg2.getCheckedRadioButtonId()))
+                        .getTag().toString();
+        int broj2=Integer.parseInt(value);
+        int broj=Integer.parseInt(value2);
         Intent intent=new Intent(this,IgraActivity.class);
         intent.putExtra("broj",broj);
+        intent.putExtra("broj2",broj2);
         startActivity(intent);
 
     }
--- app/src/main/java/com/example/kurs/aaaaaaaaaaaaa/IgraActivity.java
+++ app/src/main/java/com/example/kurs/aaaaaaaaaaaaa/IgraActivity.java
@@ -2,13 +2,16 @@ package com.example.kurs.aaaaaaaaaaaaa;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -17,7 +20,12 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.RemoteViews;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class IgraActivity extends AppCompatActivity {
 
@@ -25,31 +33,72 @@ public class IgraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_igra);
-        int nesto= getIntent().getIntExtra("broj",6);
+        final  ArrayList<Boolean> niz =new ArrayList<>();
+        final int nesto= getIntent().getIntExtra("broj",6);
+        int nesto2=getIntent().getIntExtra("broj2",5);
+
         GridLayout glmrezapolja=(GridLayout)findViewById(R.id.glMreza);
         glmrezapolja.setColumnCount(nesto);
         Display display = getWindowManager().getDefaultDisplay();
-        int height= display.getHeight()/nesto-2;
+        //int height= display.getHeight()/nesto-2;
         int width=display.getWidth()/nesto-2;
+        Log.d("ss",width+" "+display.getWidth());
+
         for(int i=0;i<nesto*nesto;i++)
         {
-            ImageButton dugme=new ImageButton(this);
+            niz.add(false);
+
+            final ImageButton dugme=new ImageButton(this);
             dugme.setBackgroundColor(Color.GRAY);
             //dugme.setImageDrawable(getResources().getDrawable(R.drawable.smajli));
             GridLayout.LayoutParams gllp= new GridLayout.LayoutParams();
-            gllp.setMargins(2,2,2,2);
-            gllp.height=height;
+            gllp.setMargins(1,1,1,1);
+            gllp.height=width;
             gllp.width=width;
             dugme.setLayoutParams(gllp);
             glmrezapolja.addView(dugme);
+            dugme.setTag(i);
+            dugme.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int trenutnapoz=(int)v.getTag();
+                    int br=0;
+                    if(niz.get(trenutnapoz))
+                    {
+                        v.setBackground(getResources().getDrawable(R.drawable.mina));
+                    }
+                    else
+                    {
+                        for(int i = trenutnapoz-nesto; i<=trenutnapoz+nesto;i+=nesto){
+                            for(int j = -1; j <= 1; j++) {
+                                if((trenutnapoz+j)/(trenutnapoz/nesto+1)<nesto
+                                        && (trenutnapoz+j)/(trenutnapoz/nesto+1)>=0
+                                        && niz.get(i+j)) {
+                                    br++;
+                                }
+                                }
+                            }
+                        }
+                        int resID=getResources().getIdentifier("d"+br,"drawable",getPackageName());
+                        if(br!=0) {
+                            v.setBackground(getResources().getDrawable(resID));
+                        }
+                    }
+            });
         }
+        for(int i=0;i<nesto2;i++)
+        {
+            niz.set(i,true);
+        }
+        Collections.shuffle(niz);
         RelativeLayout.LayoutParams llp=new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
         llp.addRule(RelativeLayout.CENTER_IN_PARENT);
         glmrezapolja.setLayoutParams(llp);
+        Log.d("sssss",glmrezapolja.getWidth()+"");
     }
 
     public DisplayMetrics getDisplay(){
         return new DisplayMetrics();
-
     }
 }
